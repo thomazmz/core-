6bfef2dd3fb0e764a48b3efcba3f23d619b717fc
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2023-04-20 18:57:37 -0700

    feat: create separate type for sort ordering

--- src/domains/sort/values/sort.ts
+++ src/domains/sort/values/sort.ts
@@ -1,6 +1,8 @@
+import { SortOrder } from './sort-order'
+
 export type Sort<T extends string> = {
   offset?: number
   limit?: number
   property?: T
-  order?: 'ascending' | 'descending'
+  order?: SortOrder
 };
