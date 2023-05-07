f4c74d4d429d33858855442fadae99511116d846
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2023-05-06 19:54:55 -0700

    fix: remove keyof type operator from entity properties to omit

--- src/domains/entity/entity-properties.ts
+++ src/domains/entity/entity-properties.ts
@@ -1,3 +1,3 @@
 import { Entity } from './entity';
 
-export type EntityProperties<E extends Entity<any>> = Omit<E, keyof 'id' | 'createdAt' | 'updatedAt'>
+export type EntityProperties<E extends Entity<any>> = Omit<E, 'id' | 'createdAt' | 'updatedAt'>
