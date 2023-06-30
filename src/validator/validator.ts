4c16f89e11d2b931b9cdd8d4f122948591126fca
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2023-06-29 21:33:14 -0700

    chore: define Validator interface

---
+++ src/validator/validator.ts
@@ -0,0 +1,31 @@
+import { Value } from '../domain'
+
+/*
+ * Validates an unknown value in accordance to a schema
+ */
+export interface Validator<V = Value> {
+  /**
+   * @description Tries to cast a value in accordance to the validator schema behaviouring as a typeguard.
+   * @returns {boolean}
+   */
+  cast(value: unknown): value is V
+
+  /**
+   * @description Validates an unkown value, returning true if validation succeeds or false if the validation fails.
+   * @returns {boolean}
+   */
+  validate(value: unknown): boolean
+
+  /**
+   * @description Validates an unknow value, returning true if validation succeeds or throwing an error if the validation fails.
+   * @returns {true}
+   * @throws {ValidationError}
+   */
+  assert(value: unknown): true | void
+
+  /**
+   * @description Checks an unknown value and returns a validation error if any.
+   * @returns {Optional<ValidationError>}
+   */
+  extractValidationError(value: unknown): Error | undefined
+}
