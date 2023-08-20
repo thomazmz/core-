ecfbe310cee4cbad02b3e736862f212d283cac16
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2023-08-20 11:28:19 -0700

    rename http status types

--- src/http/http-status.ts
+++ src/http/http-status.ts
@@ -1,88 +1,88 @@
 
-export const Ok = {
+export const HttpOk = {
   message: 'Ok',
   code: 200,
 } as const 
 
-export const Created = {
+export const HttpCreated = {
   message: 'Created',
   code: 201,
 } as const 
 
-export const Accepted = {
+export const HttpAccepted = {
   message: 'Accepted',
   code: 202,
 } as const 
 
-export const NoContent = {
+export const HttpNoContent = {
   message: 'No Content',
   code: 204,
 } as const 
 
-export const MovedPermanently = {
+export const HttpMovedPermanently = {
   message: 'Moved Permanently',
   code: 301,
 } as const 
 
-export const Found = {
+export const HttpFound = {
   message: 'Found',
   code: 302,
 } as const 
 
-export const NotModified = {
+export const HttpNotModified = {
   message: 'Not Modified',
   code: 304,
 } as const 
 
-export const BadRequest = {
+export const HttpBadRequest = {
   message: 'Bad Request',
   code: 400,
 } as const 
 
-export const Unauthorized = {
+export const HttpUnauthorized = {
   message: 'Unauthorized',
   code: 401,
 } as const 
 
-export const Forbiden = {
+export const HttpForbiden = {
   message: 'Forbiden',
   code: '403',
 } as const 
 
-export const NotFound = {
+export const HttpNotFound = {
   message: 'Not found',
   code: '404',
 } as const 
 
-export const TooManyRequests = {
+export const HttpTooManyRequests = {
   message: 'Too many requests',
   code: '429',
 } as const 
 
-export const InternalServerError = {
+export const HttpInternalServerError = {
   message: 'Internal server error',
   code: '500',
 } as const 
 
-export const ServiceNotAvailable = {
+export const HttpServiceNotAvailable = {
   message: 'Service not available',
   code: '503',
 } as const 
 
 export const HttpStatuses = [
-  Created,
-  Accepted,
-  NoContent,
-  MovedPermanently,
-  Found,
-  NotModified,
-  BadRequest,
-  Unauthorized,
-  Forbiden,
-  NotFound,
-  TooManyRequests,
-  InternalServerError,
-  ServiceNotAvailable,
+  HttpCreated,
+  HttpAccepted,
+  HttpNoContent,
+  HttpMovedPermanently,
+  HttpFound,
+  HttpNotModified,
+  HttpBadRequest,
+  HttpUnauthorized,
+  HttpForbiden,
+  HttpNotFound,
+  HttpTooManyRequests,
+  HttpInternalServerError,
+  HttpServiceNotAvailable,
  ] as const
 
 export type HttpStatus = (typeof HttpStatuses)[number]
